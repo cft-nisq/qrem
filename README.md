aefcd88a65f89eaabd046dea7477e50b1c646f5d
Jan Tuziemski <jan.tuziemski@gmail.com>
2023-11-17 19:11:40 +0100

    file_names update

--- README.md
+++ README.md
@@ -7,7 +7,20 @@ in readout noise in near-term quantum devices"](arxiv link). In order to perform
 
 2. Download experimental data available [here](https://drive.google.com/drive/folders/14Jh3gJUbiipVLVoWSugJ4uYcZpZWd9XS?usp=drive_link) 
 
-3. Set paths to experimental data in the article_data_analysis.py
+3. Set paths to experimental data in the article_data_analysis.py:
+
+    * DATA_DIRECTORY: directory with experimental data 
+
+    * FILE_NAME_RESULTS_IBM and FILE_NAME_RESULTS_RIG: files with raw experimental results, on Google drive linked above RESULTS_IBM_CUSCO.pkl and RESULTS_RIGETTI_ASPEN-M-3.pkl
+
+    * FILE_NAME_GROUND_STATES_LIST_IBM and FILE_NAME_GROUND_STATES_LIST_RIG: files with ground states used in benchmarks, on Google drive linked above   IBM_CUSCO_GROUND_STATES_LIST.pkl and RIGETTI-ASPEN-M-3_GROUND_STATES_LIST,pkl 
+ 
+    * COHERENCE_WITNESS_CIRCUITS_PATH_IBM and COHERENCE_WITNESS_CIRCUITS_PATH_RIG: files with states used to compute Coherence Strength, on Google drive linked above IBM_CUSCO_COHERENCE_WITNESS_CIRCUITS.PKL and RIGETTI-ASPEN-M-3_COHERENCE_WITNESS_CIRCUITS 
+
+    * FILE_NAME_HAMILTONIANS_IBM and FILE_NAME_HAMILTONIANS_RIG iles with storing Hamiltonians used in benchmarks, on Google drive linked above IBM_CUSCO_HAMILTONIANS_DICTIONARY.pkl AND RIGETTI-ASPEN-M-3_HAMILTONIANS_DICTIONARY.pkl
+
+    * FILE_NAME_MARGINALS_IBM and FILE_NAME_MARGINALS_RIG: Optionally pre-processed files with 2-qubit marginal data can be loaded, on Google drive linked above IBM_CUSCO_HAMILTONIANS_DICTIONARY.pkl AND RIGETTI-ASPEN-M-3_HAMILTONIANS_DICTIONARY.pkl. This data can by omitted and, after suitable modification of the code, recomputed in the script
+
 
 4. Specify path where the results will be saved 
 
--- article_data_analysis.py
+++ article_data_analysis.py
@@ -3,29 +3,10 @@ from qrem.pipelines.characterization_routine import execute_characterization_wor
 from  qrem.pipelines.mitigation_functions import estimate_mitigated_energy_over_noise_models
 from qrem.pipelines.mitigation_functions import compute_noisy_energy_over_noise_models
 from qrem.visualisation import article_plots_functions as apf
-import numpy as np
 from qrem.functions_qrem import functions_benchmarks as fun_ben
-from qrem.functions_qrem import ancillary_functions as anf
 from qrem.common.printer import qprint
-
-
-from matplotlib import pyplot as plt
-
 from qrem.common import io
-
-
-
-
-import sys
-import os
-
-import numpy as np
 import pickle
-
-
-
-from datetime import date
-from typing import Dict
 
 
 
@@ -37,26 +18,23 @@ from typing import Dict
   
  
 #set path to data directory
-DATA_DIRECTORY= '/media/tuzjan/T7/work_tuzjan/experiments_ibm/ibm_cusco_2023-10-01/'
+DATA_DIRECTORY= ''
 
 #set name of file where bare counts are stored 
-FILE_NAME_RESULTS_IBM =  'ibm_cusco_results.pkl'
+FILE_NAME_RESULTS_IBM =  'RESULTS_IBM_CUSCO.pkl'
 
 #set name of file with ground states list, this is the states used in benchmarks
-FILE_NME_GROUND_STATES_LIST_IBM = '/media/tuzjan/T7/work_tuzjan/ground_state_hamiltonians/ibm_ground_states_list.pkl'
+FILE_NAME_GROUND_STATES_LIST_IBM = 'IBM_CUSCO_GROUND_STATES_LIST.pkl '
 
 #set name of file with states list used to calculate coherence strength 
-COHERENCE_STRENGTH_CIRCUITS_PATH_IBM = '/media/tuzjan/T7/work_tuzjan/coherence witness circuits/coherence_witness_circuits_all_combinations_q127.pkl'
+COHERENCE_WITNESS_CIRCUITS_PATH_IBM = 'IBM_CUSCO_COHERENCE_WITNESS_CIRCUITS.pkl'
 
-#
 
-DIRECTORY_HAMILTONIANS= '/media/tuzjan/T7/work_tuzjan/ground_state_hamiltonians/'
-
-FILE_NAME_HAMILTONIANS_IBM = 'hamiltonians_dictionary_600_hamiltonian_127_qubits.pkl'
+FILE_NAME_HAMILTONIANS_IBM = 'IBM_CUSCO_HAMILTONIANS_DICTIONARY.pkl'
 
 
 #Optional: set name of file where marginal probability distributions are stored, if they were calculated previously
-FILE_NAME_MARGINALS_IBM ='DDOT_marginals_workflow_2023-10-04 q127.pkl'
+FILE_NAME_MARGINALS_IBM ='IBM_CUSCO_MARGINALS_DICTIONARY.pkl'
 
 
 
@@ -67,15 +45,15 @@ with open(DATA_DIRECTORY + FILE_NAME_RESULTS_IBM, 'rb') as filein:
     results_dictionary_ibm = pickle.load(filein)
 
 #ground states list us loaded
-with open(FILE_NME_GROUND_STATES_LIST_IBM, 'rb') as filein:
+with open(DATA_DIRECTORY+FILE_NAME_GROUND_STATES_LIST_IBM, 'rb') as filein:
     circuits_ground_states_preparation_collection_ibm= pickle.load( filein)
 
 #coherence strength circuits are loaded 
-with open(COHERENCE_STRENGTH_CIRCUITS_PATH_IBM , 'rb') as filein:
+with open(DATA_DIRECTORY+COHERENCE_WITNESS_CIRCUITS_PATH_IBM , 'rb') as filein:
     coherence_witness_circuits_ibm =  pickle.load(filein)
 
 #Optional: marginals are loaded
-with open(DIRECTORY_HAMILTONIANS + FILE_NAME_HAMILTONIANS_IBM, 'rb') as filein:
+with open(DATA_DIRECTORY+ FILE_NAME_HAMILTONIANS_IBM, 'rb') as filein:
     hamiltonians_dictionary_ibm = pickle.load(filein)
 
 #Optional: marginals are loaded
@@ -85,20 +63,18 @@ with open(DATA_DIRECTORY + FILE_NAME_MARGINALS_IBM, 'rb') as filein:
 marginals_dictionary_ibm =marginals_dictionary_ibm['marginals_dictionary']
         
 
-DATA_DIRECTORY_RIG = '/media/tuzjan/T7/work_tuzjan/experiments_rigetti/'
-
-FILE_NAME_RESULTS_RIG =  'rigetti_results.pkl'
-
-FILE_NAME_MARGINALS_RIG = 'DDOT_marginals_workflow_2023-09-29 q79Rigetti_4.pkl'
-
-COHERENCE_WITNESS_CIRCUITS_PATH_RIG = '/media/tuzjan/T7/work_tuzjan/coherence witness circuits/coherence_witness_circuits_q79.pkl'
 
 
-FILE_NAME_HAMILTONIANS_RIG = 'hamiltonians_dictionary_100_hamiltonian_79_qubits.pkl'
+FILE_NAME_RESULTS_RIG =  'RESULTS_RIGETTI_ASPEN-M-3.pkl'
 
-FILE_NAME_GROUND_STATES_LIST_RIG = '/media/tuzjan/T7/work_tuzjan/ground_state_hamiltonians/hamiltonians_dictionary_50_gorund_states_79_qubits.pkl'
+FILE_NAME_GROUND_STATES_LIST_RIG = 'RIGETTI-ASPEN-M-3_GROUND_STATES_LIST.pkl'
 
-GROUND_STATES_NUMBER = 50
+COHERENCE_WITNESS_CIRCUITS_PATH_RIG = 'RIGETTI-ASPEN-M-3_COHERENCE_WITNESS_CIRCUITS.pkl'
+
+FILE_NAME_HAMILTONIANS_RIG = 'RIGETTI-ASPEN-M-3_HAMILTONIANS_DICTIONARY.PKL'
+
+FILE_NAME_MARGINALS_RIG = 'RIGETTI-ASPEN-M-3_MARGINALS_DICTIONARY.pkl'
+
 
 
 
@@ -106,23 +82,23 @@ GROUND_STATES_NUMBER = 50
 
 
 #experimental results are loaded
-with open(DATA_DIRECTORY_RIG + FILE_NAME_RESULTS_RIG, 'rb') as filein:
+with open(DATA_DIRECTORY + FILE_NAME_RESULTS_RIG, 'rb') as filein:
     results_dictionary_rig = pickle.load(filein)
 
 #ground states list us loaded
-with open(FILE_NAME_GROUND_STATES_LIST_RIG, 'rb') as filein:
+with open(DATA_DIRECTORY+FILE_NAME_GROUND_STATES_LIST_RIG, 'rb') as filein:
     circuits_ground_states_preparation_collection_rig= pickle.load( filein)
 
 #coherence strength circuits are loaded 
-with open(COHERENCE_WITNESS_CIRCUITS_PATH_RIG  , 'rb') as filein:
+with open(DATA_DIRECTORY+COHERENCE_WITNESS_CIRCUITS_PATH_RIG  , 'rb') as filein:
     coherence_witness_circuits_rig =  pickle.load(filein)
 
 #Optional: marginals are loaded
-with open(DIRECTORY_HAMILTONIANS + FILE_NAME_HAMILTONIANS_RIG, 'rb') as filein:
+with open(DATA_DIRECTORY + FILE_NAME_HAMILTONIANS_RIG, 'rb') as filein:
     hamiltonians_dictionary_rig = pickle.load(filein)
 
 #Optional: marginals are loaded
-with open(DATA_DIRECTORY_RIG + FILE_NAME_MARGINALS_RIG, 'rb') as filein:
+with open(DATA_DIRECTORY + FILE_NAME_MARGINALS_RIG, 'rb') as filein:
     marginals_dictionary_rig = pickle.load(filein)
 
 
@@ -162,16 +138,7 @@ if __name__ == "__main__":
 
 
 
-    #data_dictionary = io.load('/media/tuzjan/T7/work_tuzjan/tests/2023-11-16 qibm_rig.pkl')
-
-    #characterization_routine_results_dictionary_ibm= data_dictionary['characterization_dictionary_ibm' ]
-
-    #characterization_routine_results_dictionary_rig= data_dictionary['characterization_dictionary_rig' ]
-
-    #noise_models_mitigated_energy_dictionary_rig=data_dictionary['noise_models_mitigated_energy_dictionary_rig']
-
-    #noise_models_mitigated_energy_dictionary_ibm=data_dictionary['noise_models_mitigated_energy_dictionary_ibm']
-
+ 
 
     noise_models_predicted_energy_dictionary_ibm = compute_noisy_energy_over_noise_models(results_dictionary=characterization_routine_results_dictionary_ibm['benchmarks_results_dictionary'],hamiltonians_dictionary=hamiltonians_dictionary_ibm,noise_models_list=characterization_routine_results_dictionary_ibm['noise_models_list'])
 
