1d99b796563eebab01199ed96b6cfdb414a22324
piotr@3dfy.ai <piotr@3dfy.ai>
2023-11-17 12:07:16 +0100

    Working on github autoapi docs

--- src/qrem/__init__.py
+++ src/qrem/__init__.py
@@ -1,11 +1,11 @@
 #ORGANIZATION - dont know what is the purpose of this file (MO) - (PP): important for defining initialization for packages/subpackages in python (think of every folder as a package/subpackage, when you import it __init__.py is run)
-"""**qrem*** module is the main module of the whole package.
-It contains all qrem code after refactoring into version 1.0.0. Currently it holds
+"""**qrem** module is the main module of the whole package.
+It contains all qrem code after refactoring into upcoming version 1.0.0. Currently it holds
 most important data structures and common functions used in the freamework. The main subpackages are: \n\n
 - **common**: contains all common functions, utilities, connection to quantum backends etc. used throughout whole qrem package\n
 - **cn**: csubpackage containing cluster-neighbours approach implementation \n
 - **ctmp**: Mitigation procedure as described in Bravyi et al. \n
--** pipelines**: scripts containing executions of most common characterisation/mitigation routines from data generation, sending to quantum machine down to characterization and mitigation. \n
+- **pipelines**: scripts containing executions of most common characterisation/mitigation routines from data generation, sending to quantum machine down to characterization and mitigation. \n
 - **types**: most important data holder classess used throughout the package \n
 """ 
 
