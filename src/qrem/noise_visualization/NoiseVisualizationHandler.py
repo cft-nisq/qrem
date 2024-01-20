38587b9f22e4bd40aeaa9221a1f3df30ebeab56f
piotr@3dfy.ai <piotr@3dfy.ai>
2024-01-20 16:31:27 +0100

    migrating updates from refactoring repo #2

--- src/qrem/noise_visualization/NoiseVisualizationHandler.py
+++
@@ -1,7 +0,0 @@
-"""
-Notes
------
-    @authors: Jan Tuziemski, Filip Maciejewski, Joanna Majsak, Oskar Słowik, Marcin Kotowski, Katarzyna Kowalczyk-Murynka, Paweł Przewłocki, Piotr Podziemski, Michał Oszmaniec
-    @contact: michal.oszmaniec@cft.edu.pl
-"""
-#ORGANIZE - this file seams obsolete (MO)
